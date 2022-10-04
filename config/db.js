986f45f7f7455e7e6758a8bd6f36e1f3d7a5de8c
azacy <enejustin725@gmail.com>
2022-10-04 12:59:07 +0100

    changes mongoose.connect to the url same with utils

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const connectDB = async() => {
     try {
-        mongoose.connect(process.env.MONGODB_DEV)
+        mongoose.connect("mongodb+srv://comm:85218521@cluster0.f6dvdlz.mongodb.net/test")
         console.log(`Connected to MongoDB Successfully`);
     } catch (error) {
         console.error(`Error: ${error.message}`)
--- utility/utility.js
+++ utility/utility.js
@@ -1,7 +1,7 @@
 import  jwt  from "jsonwebtoken";
 
  export const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_PASSKEY, {
+    return jwt.sign({id}, "56A56251436C044434190BE8198969EFD4303C24893331B3A04B8F85D5388500", {
         expiresIn: '30d'
     })
  }
